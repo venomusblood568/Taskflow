c09ba40b5a183f7b533e105eea8e8c63a7400be2
GOURAV_ANAND_JHA <90522738+venomusblood568@users.noreply.github.com>
2025-06-15 13:23:29 +0530

    Fe setup Done

---
+++ Fe/src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+   <>
+   <div className='text-blue-600 uppercase'>
+    <h1>TaskFLow</h1>
+   </div>
+   </>
+  )
+}
+
+export default App
